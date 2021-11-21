815cc3146908ed9573bdb88cbc392abd72da77e5
f-invidia98 <francesco.invidia.98@gmail.com>
2021-11-21 13:57:33 +0100

    tutto

---
+++ greenway_script.js
@@ -0,0 +1,9 @@
+$(".click").click(function(){
+  $("#menu1").addClass("hide")
+  $("#menu2").removeClass("hide")
+})
+
+$(".click2").click(function(){
+  $("#menu2").addClass("hide")
+  $("#menu1").removeClass("hide")
+})
